499546bf7afd7d76177b2c3f86d2bdfec86dcf16
leejihoon <ljhljh211@naver.com>
2023-08-25 22:39:39 +0900

    feat: 아티클 상세 하단 공통 링크 공유 섹션 추가

--- view/article/articlePage.js
+++ view/article/articlePage.js
@@ -2,12 +2,14 @@ import footer from "../ui/footer.js";
 import header from "../ui/header.js";
 import bottomBanner from "../ui/bottomBanner.js";
 import articleHeader from "./articleHeader.js";
+import articleBottom from "./articleBottom.js";
 
 const articlePage = (res) => {
   const { thumbImg, article_id, title, desc, writer, reg_date, content } = res;
   const headerTemplate = header();
   const footerTemplate = footer();
   const bottomBannerTemplate = bottomBanner();
+  const articleBottomTemplate = articleBottom();
   const headerData = {
     thumbImg,
     title,
@@ -21,6 +23,12 @@ const articlePage = (res) => {
 
   <section class='px-[30px] max-w-[700px] mb-[192px] mx-auto'>
     {{__article__header__}}
+
+    {{__article_content__}}
+    
+  </section>
+  <section class="max-w-[700px] mx-auto">
+    {{__article__bottom__}}
   </section>
   
   {{__bottom_banner__}}
@@ -29,8 +37,9 @@ const articlePage = (res) => {
 
   template = template.replace("{{__header__}}", headerTemplate);
   template = template.replace("{{__article__header__}}", articleHeaderTemplate);
-  template = template.replace("{{__footer__}}", footerTemplate);
+  template = template.replace("{{__article__bottom__}}", articleBottomTemplate);
   template = template.replace("{{__bottom_banner__}}", bottomBannerTemplate);
+  template = template.replace("{{__footer__}}", footerTemplate);
 
   return template;
 };
