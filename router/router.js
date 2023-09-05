b6737f927ae5a5a0d4b7d4e4eb4402960aaf07df
leejihoon <ljhljh211@naver.com>
2023-09-05 21:16:09 +0900

    comment: router.js 코멘트 추가

--- router/router.js
+++ router/router.js
@@ -1,5 +1,19 @@
 const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
 const URL_FRAGMENT_REGEXP = "([^\\/]+)";
+
+/* 
+위 두 정규표현식으로
+예)  /article/:id라는 패턴을 가진 라우트를 등록할 경우
+ROUTE_PARAMETER_REGEXP는 :id를 감지하고 이를 URL_FRAGMENT_REGEXP로 교체
+이는 /article/([^\\/]+) 정규ㅠㅛ현식이 되며, 'article/123' 같은 실제 URL과 매치됨
+*/
+
+/**
+ * 현재 URL에서 동적 라우트 매개변수의 값을 추출
+ * @param {*} route - 주어진 라우트
+ * @param {*} pathname - 경로명을 기반으로
+ * @returns {{}} params 파라미터 추출
+ */
 const extractUrlParams = (route, pathname) => {
   const params = {};
   if (route.params.length === 0) {
@@ -15,9 +29,9 @@ const extractUrlParams = (route, pathname) => {
 };
 
 class Router {
-  routes;
-  notFound;
-  lastPathname;
+  #routes;
+  #notFound;
+  #lastPathname;
 
   constructor() {
     this.routes = [];
@@ -25,6 +39,10 @@ class Router {
     this.lastPathname = "";
   }
 
+  /* 
+  현재 URL을 확인하고 일치하는 라우트의 콜백을 실행
+  일치하는 라우트 없을 경우 notFound 콜백 실행
+  */
   checkRoutes() {
     const { pathname } = window.location;
     if (this.lastPathname === pathname) {
@@ -45,6 +63,13 @@ class Router {
     currentRoute.callback(urlParams);
   }
 
+  /**
+   * routes 객체에 path, 콜백함수 인자로 받아
+   *
+   * @param {*} path - url, url/:경로매개변수
+   * @param {*} callback - 렌더함수
+   * @returns router 객체에 경로, 해당경로에 대한 콜백 등록
+   */
   addRoute(path, callback) {
     const params = [];
     const parsedPath = path
@@ -61,16 +86,19 @@ class Router {
     return this;
   }
 
+  /* 일치하는 라우트 없을 떄 실행할 콜백 설정 */
   setNotFound(cb) {
     this.notFound = cb;
     return this;
   }
 
+  /* 주어진 경로로 이동하고 해당 라우트의 콜백을 실행 */
   navigate(path) {
     window.history.pushState(null, null, path);
     this.checkRoutes();
   }
 
+  /** 초기화 */
   init() {
     this.checkRoutes();
     window.addEventListener("popstate", this.checkRoutes.bind(this));
